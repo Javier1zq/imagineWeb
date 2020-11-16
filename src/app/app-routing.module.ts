e3fa563e2fc58a0700b579df8b709328c5ab8cde
Javier1zq <izquierdoj98@gmail.com>
2020-11-16 12:41:04 +0100

    Added home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
+import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+     component: PublicComponent,
+     children:[
+       {path: '', component: HomeComponent}
+     ]
+    },
+  {path: 'secure', component: SecureComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
+import { HomeComponent } from './public/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    SecureComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
