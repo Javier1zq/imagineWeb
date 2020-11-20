3c586facfa85efbd23607087cd0594976d8b55fa
Javier1zq <izquierdoj98@gmail.com>
2020-11-20 17:08:52 +0100

    register works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { LoginComponent } from './public/login/login.component';
+import { RegisterComponent } from './public/register/register.component';
 import { PublicComponent } from './public/public.component';
 import { SecureComponent } from './secure/secure.component';
 
@@ -12,7 +13,9 @@ const routes: Routes = [
      component: PublicComponent,
      children:[
        {path: '', component: HomeComponent},
-       {path: 'login', component: LoginComponent}
+       {path: 'login', component: LoginComponent},
+       {path: 'register', component: RegisterComponent}
+
      ]
     },
   {path: 'secure', component: SecureComponent}
--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb: FormBuilder,
+              private http: HttpClient,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.form= this.fb.group({
+      first_name:['',Validators.required],
+      last_name:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      password:['',Validators.required],
+      password_confirmation:['',Validators.required]
+    });
   }
-
+  submit(){
+    const formData = this.form.getRawValue();
+    this.http.post('http://localhost:8000/api/register', formData).subscribe(
+      (result) =>{
+        console.log(result);
+      },
+      error =>{
+        console.log(error);
+        console.log('hello');
+      }
+    )
+  };
 }
